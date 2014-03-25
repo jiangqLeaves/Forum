3489b064f08a16994033485c020604a42691941d
jiangqLeaves <jiangq.leaves@gmail.com>
2014-03-25 14:51:50 +0800

    增加topic服务

--- public/javascripts/topicEditCtrl.js
+++ public/javascripts/topicEditCtrl.js
@@ -1,8 +1,9 @@
-﻿app.controller('topicEditCtrl', ['$scope','$sanitize','$routeParams', function ($scope,$sanitize,$routeParams) {
+﻿app.controller('topicEditCtrl', ['$scope','$sanitize','$routeParams','Topic', function ($scope,$sanitize,$routeParams,Topic) {
     if(!$routeParams.id)
         alert("ERRO")
     else
     alert($routeParams.id);
+    $scope.topicDetail=Topic.get({topicId:$routeParams.id})
     $scope.contents='你可以使用 **Markdown** 在这里进行编写';
       $scope.$watch('contents',function(){
           $scope.contentHtml=markdown.toHTML($scope.contents);
